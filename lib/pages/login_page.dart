5ca608866359412ea7eb12eef57f5703eedcf269
anaviel <anaviele@mail.ru>
2024-09-13 18:54:57 +0300

    fixed exit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vers2/design/colors.dart';
-import 'package:vers2/pages/home_page.dart';
 import 'navigation.dart';
 import 'signup.dart';
 import 'new_password.dart'; // Импортируем экран восстановления пароля
@@ -18,128 +16,125 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: accentColor),
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.white,
-        body: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      const Padding(
-                        padding: EdgeInsets.only(top: 120),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.only(top: 120),
+                    ),
+                    Image.asset(
+                      'assets/img/logo.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                    ),
+                    const Text("Вход",
+                        style: TextStyle(
+                            fontSize: 35,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 30),
+                    ),
+                    buildLoginTextField('Логин', _loginController),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                    ),
+                    buildTextField('Пароль', _passwordController),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                    ),
+                    MaterialButton(
+                      minWidth: 150,
+                      height: 60,
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Navigation()),
+                              (Route<dynamic> route) => false,
+                        ); // Полностью очищаем стек навигации
+                      },
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
                       ),
-                      Image.asset(
-                        'assets/img/logo.png',
-                        width: 100,
-                        height: 100,
+                      child: const Text(
+                        "Войти",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 20),
-                      ),
-                      const Text("Вход",
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: blackColor,
-                              fontWeight: FontWeight.bold)),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 30),
-                      ),
-                      buildLoginTextField('Логин', _loginController),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 20),
-                      ),
-                      buildTextField('Пароль', _passwordController),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 20),
-                      ),
-                      MaterialButton(
-                        minWidth: 150,
-                        height: 60,
-                        onPressed: () {
-                          Navigator.pushReplacement(
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => const Navigation()));
-                        },
-                        color: accentColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: const Text(
-                          "Войти",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 20),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                    const NewPasswordPage()), // Навигация к экрану восстановления пароля
-                              );
-                            },
-                            child: const Text(
-                              "Забыли пароль?",
-                              style: TextStyle(
-                                color: greyColor,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 20),
-                          const Text(
-                            "Нет аккаунта? ",
+                                  builder: (context) =>
+                                  const NewPasswordPage()), // Навигация к экрану восстановления пароля с заменой страницы
+                            );
+                          },
+                          child: const Text(
+                            "Забыли пароль?",
                             style: TextStyle(
-                              color: greyColor,
+                              color: Colors.grey,
                               fontSize: 18,
                             ),
                           ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                    const SignUpPage()),
-                              );
-                            },
-                            child: const Text(
-                              "Зарегистрироваться",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 18,
-                              ),
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          "Нет аккаунта? ",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpPage()),
+                            ); // Переход на страницу регистрации с заменой текущей страницы
+                          },
+                          child: const Text(
+                            "Зарегистрироваться",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -151,7 +146,7 @@ class _LoginPageState extends State<LoginPage> {
       constraints: const BoxConstraints(maxWidth: 400),
       decoration: BoxDecoration(
         color: Colors.white,
-        border: Border.all(color: greyColor),
+        border: Border.all(color: Colors.grey),
         borderRadius: BorderRadius.circular(12),
       ),
       child: Padding(
@@ -162,7 +157,7 @@ class _LoginPageState extends State<LoginPage> {
             border: InputBorder.none,
             hintText: hintText,
             hintStyle: const TextStyle(
-              color: greyColor,
+              color: Colors.grey,
               fontSize: 18,
             ),
           ),
@@ -177,7 +172,7 @@ class _LoginPageState extends State<LoginPage> {
       constraints: const BoxConstraints(maxWidth: 400),
       decoration: BoxDecoration(
         color: Colors.white,
-        border: Border.all(color: greyColor),
+        border: Border.all(color: Colors.grey),
         borderRadius: BorderRadius.circular(12),
       ),
       child: Padding(
@@ -188,7 +183,7 @@ class _LoginPageState extends State<LoginPage> {
           decoration: InputDecoration(
             border: InputBorder.none,
             hintText: hintText,
-            hintStyle: const TextStyle(color: greyColor, fontSize: 18),
+            hintStyle: const TextStyle(color: Colors.grey, fontSize: 18),
           ),
         ),
       ),
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
 import 'package:postgres/postgres.dart';
+import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginPage(),
-  ));
 
-  // final conn = PostgreSQLConnection(
-  //   '10.0.2.2',
-  //   5432,
-  //   'vuzhub_db',
-  //   username: 'postgres',
-  //   password: 'mahkamov03',
-  // );
-  //
-  // try {
-  //   await conn.open();
-  //   print('Connected to database');
-  // } catch (e) {
-  //   print('Error executing query: $e');
-  // } finally {
-  //   await conn.close();
-  // }
+  // Подключаемся к базе данных
+  await initializeDatabase();
+
+  // Запускаем приложение
+  runApp(const MyApp());
+}
+
+Future<void> initializeDatabase() async {
+  final conn = PostgreSQLConnection(
+    '10.0.2.2', // Локальный хост для эмулятора Android
+    5432,
+    'vuzhub_db',
+    username: 'postgres',
+    password: 'mahkamov03',
+  );
+
+  try {
+    await conn.open();
+    print('Connected to database');
+  } catch (e) {
+    print('Error connecting to the database: $e');
+  } finally {
+    await conn.close();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
 }
